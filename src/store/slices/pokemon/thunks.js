332b7552c101376cad7096e6053ce8ee031d7011
Marcel Cabrera <marcel.cabrera@gmail.com>
2022-10-14 01:02:28 -0500

    RTK - Thunks changed from fetch to axios

--- src/store/slices/pokemon/thunks.js
+++ src/store/slices/pokemon/thunks.js
@@ -1,12 +1,13 @@
+import axios from "axios";
+import { pokemonApi } from "../../../api/pokemonApi";
 import { startLoadingPokemons, setPokemons } from "./"
 
 export const getPokemons = (page = 0) =>{
     return async (dispatch, getState) =>{
         dispatch(startLoadingPokemons());
 
-        const resp = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${page*10}`);
-        const data = await resp.json();
+        const { data } = await pokemonApi.get(`pokemon?limit=10&offset=${page*10}`);
 
-         dispatch(setPokemons({page: page + 1, pokemons: data.results}))
+        dispatch(setPokemons({page: page + 1, pokemons: data.results}))
     }
 }
